68fe40b9cb1df1e08a02df3bdd4e4057811ff5fc
Ibrahim Irsad <63491368+Ibrairsyad17@users.noreply.github.com>
2024-07-17 00:51:07 +0700

    fix: image carousel

--- app/components/MoreSection.tsx
+++ app/components/MoreSection.tsx
@@ -8,6 +8,33 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 
+const carouselImages = [
+  {
+    src: "/assets/img/image-1.jpg",
+    alt: "image-1",
+  },
+  {
+    src: "/assets/img/image-2.jpg",
+    alt: "image-2",
+  },
+  {
+    src: "/assets/img/image-3.jpg",
+    alt: "image-3",
+  },
+  {
+    src: "/assets/img/image-4.jpg",
+    alt: "image-4",
+  },
+  {
+    src: "/assets/img/image-5.jpg",
+    alt: "image-5",
+  },
+  {
+    src: "/assets/img/image-6.jpg",
+    alt: "image-6",
+  },
+];
+
 const MoreSection = () => {
   return (
     <section className="w-full py-8">
@@ -23,24 +50,16 @@ const MoreSection = () => {
         </div>
         <Carousel className="w-full">
           <CarouselContent className="-ml-1">
-            {Array.from({ length: 6 }).map((_, index) => (
+            {carouselImages.map((img, index) => (
               <CarouselItem key={index} className="pl-1 md:basis-1/2">
                 <div className="p-1 overflow-y-hidden h-[20rem] lg:h-[30rem] rounded-lg">
-                  {/* <Card className="h-96 flex justify-center items-center"> */}
-                  {/* <CardContent
-                      className={`flex items-center justify-center p-6 bg-[url('/assets/img/image-${
-                        index + 1
-                      }.jpg')]`}
-                    > */}
                   <Image
-                    src={`/assets/img/image-${index + 1}.jpg`}
+                    src={img.src}
                     height={1000}
                     width={1000}
-                    alt={`image-${index + 1}`}
+                    alt={img.alt}
                     className="rounded-lg"
                   />
-                  {/* </CardContent> */}
-                  {/* </Card> */}
                 </div>
               </CarouselItem>
             ))}
